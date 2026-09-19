b4a958081933404251f94455fde4fdb731ae5b35
hirakida <hirakida29@gmail.com>
2026-09-19 14:23:47 +0900

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    application
+    checkstyle
+}
+
+repositories {
+    mavenCentral()
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+application {
+    mainClass = "org.example.App"
+}
---
+++ app/src/main/java/org/example/App.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import java.util.Map;
+import java.util.Set;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println(Map.of("Hello", "World!"));
+        System.out.println(Set.of("Hello", "World!"));
+    }
+}
